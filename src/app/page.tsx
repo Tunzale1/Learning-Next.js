69171a40b5b201deafaa17b132ee57a9123ddfc4
Tunzale1 <tunzaleimanova03@mail.ru>
2024-08-27 14:27:21 +0400

    Link Component Navigation - lesson 19

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import Link from "next/link"
+
 export default function Home() {
-  return <h1>Home Page, Welcome home!</h1>
+  return <><h1>Home Page, Welcome home!</h1>
+  <Link href="./blog"> BLog</Link>
+  <Link href="./products">Products</Link>
+  </>
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,15 @@
+import Link from "next/link"
 export default function PageList() {
+  const productId = 100
   return (
   <>
-  <h1>Product List</h1>
-  <h2>Product 1</h2>
-  <h2>Product 2</h2>
-  <h2>Product 3</h2>
+  <Link href="/">Home</Link>
+  <h1>Products</h1>
+  <h2><Link href="products/1">Product 1</Link></h2>
+  <h2><Link href="products/2">Product 2</Link></h2>
+  <h2><Link href="products/3">Product 3</Link></h2>
+  <h2><Link href={`products/${productId}`}>Product 100</Link></h2>
+
   </>
   )
 }
